4170cdb7951f63bf895ebfe993c2f5f2d5f2069b
R-Horta <103232670+R-Horta@users.noreply.github.com>
2022-05-23 18:44:04 -0300

    Speed ​​Limit Offset adjustments according to the tolerance of Brazilian roads

--- selfdrive/controls/lib/speed_limit_controller.py
+++ selfdrive/controls/lib/speed_limit_controller.py
@@ -13,9 +13,9 @@ from selfdrive.modeld.constants import T_IDXS
 
 _PARAMS_UPDATE_PERIOD = 2.  # secs. Time between parameter updates.
 _TEMP_INACTIVE_GUARD_PERIOD = 1.  # secs. Time to wait after activation before considering temp deactivation signal.
-
+# In Brazil, the margin of error for radars is 7%, according to Contran resolution 798.
 # Lookup table for speed limit percent offset depending on speed.
-_LIMIT_PERC_OFFSET_V = [0.1, 0.05, 0.038]  # 55, 105, 135 km/h
+_LIMIT_PERC_OFFSET_V = [0.0, 0.04, 0.034, 0.0375, 0.04, 0.042] # 40, 52, 62, 83, 104, 125 Km/h - old value = [0.1, 0.05, 0.038]
 _LIMIT_PERC_OFFSET_BP = [13.9, 27.8, 36.1]  # 50, 100, 130 km/h
 
 SpeedLimitControlState = log.LongitudinalPlan.SpeedLimitControlState
